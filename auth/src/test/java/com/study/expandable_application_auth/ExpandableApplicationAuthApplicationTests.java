58b6f5222f1193e342b2b135c120100bb6dee0b3
Jeongyounghyeon <snorlax1106@naver.com>
2025-03-11 20:39:22 +0900

    feat: auth service docker 환경 구축

---
+++ auth/src/test/java/com/study/expandable_application_auth/ExpandableApplicationAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.expandable_application_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpandableApplicationAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
