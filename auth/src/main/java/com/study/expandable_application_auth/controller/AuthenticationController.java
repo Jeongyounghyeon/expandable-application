8c66d30b447f82d0456defe3d3021b59ceed1bf1
Jeongyounghyeon <snorlax1106@naver.com>
2025-03-12 18:25:31 +0900

    feat: ID 존재 여부 확인 기능 구현

--- auth/src/main/java/com/study/expandable_application_auth/controller/AuthenticationController.java
+++ auth/src/main/java/com/study/expandable_application_auth/controller/AuthenticationController.java
@@ -5,15 +5,18 @@ import com.study.expandable_application_auth.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
+    @GetMapping("/exist/{id}")
+    public ResponseEntity<Boolean> isExistId(@PathVariable String id) {
+        return ResponseEntity.ok(authenticationService.isExistId(id));
+    }
 
     @PostMapping
     public ResponseEntity<Void> createAuthentication(@RequestBody AuthenticationCreateRequest request) {
